928655815242c4f8440b62af98df052b523d1ce4
Rangga Yustian Mangindaan <rangga.yustian@gmail.com>
2023-02-07 18:57:31 +0700

    feature-add-migration-schemes

---
+++ database/migrations/2023_02_07_112938_create_product_image_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductImageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_image', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('product_id');
+            $table->unsignedInteger('image_id');
+            $table->timestamps();
+
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->foreign('image_id')->references('id')->on('images');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_image');
+    }
+}
